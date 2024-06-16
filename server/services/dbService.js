e83d6438d51f60fee455d8d269951a96c0df48e5
ryuker <lifeisagamenotaproduct@gmail.com>
2024-06-16 18:54:44 +0200

    some tweaks to db service while trying to get authentication to work

--- server/services/dbService.js
+++ server/services/dbService.js
@@ -3,15 +3,21 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-      const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/AppDB`, {
-        user: 'mongo_admin',
-        pass: 'example_pass'
-      }
-    );
+      const conn = await mongoose.connect(`mongodb://127.0.0.1:8080/AppDB`);
+
+      // with login
+      // const conn = await mongoose.connect(`mongodb://127.0.0.1:8080/AppDB`, {
+      //   user: 'mongo_admin',
+      //   pass: 'example_pass'
+      // });
+      
+      // shorter version
+      // const conn = await mongoose.connect(`mongodb://mongo_admin:example_pass@127.0.0.1:8080/`);
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
-    
+
   } catch(err){
+    console.log(err);
     console.log('Connection to mongodb refused');
   }
 };
