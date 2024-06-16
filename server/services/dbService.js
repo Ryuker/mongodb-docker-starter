1bb8eb8afaa28be5ba1f804e161a05dca2b31a3b
ryuker <lifeisagamenotaproduct@gmail.com>
2024-06-16 20:06:32 +0200

    back to basic login

--- server/services/dbService.js
+++ server/services/dbService.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-      const conn = await mongoose.connect(`mongodb://127.0.0.1:8080/AppDB`);
+      const conn = await mongoose.connect(`mongodb://127.0.0.1:8080/`);
 
       // with login
-      // const conn = await mongoose.connect(`mongodb://127.0.0.1:8080/AppDB`, {
+      // const conn = await mongoose.connect(`mongodb://127.0.0.1:8080/`, {
       //   user: 'mongo_admin',
       //   pass: 'example_pass'
       // });
-      
+
       // shorter version
       // const conn = await mongoose.connect(`mongodb://mongo_admin:example_pass@127.0.0.1:8080/`);
 
