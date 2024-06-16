1d5b961aa4b07d17db4a0a888118ce68eb512dc5
ryuker <lifeisagamenotaproduct@gmail.com>
2024-06-16 18:22:43 +0200

    added try catch to handle db connection error

--- server/services/dbService.js
+++ server/services/dbService.js
@@ -1,14 +1,19 @@
-const mongoose = require('colors');
+const colors = require('colors');
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/AppDB`, {
-      user: 'mongo_admin',
-      pass: 'example_pass'
-    }
-  );
+  try {
+      const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/AppDB`, {
+        user: 'mongo_admin',
+        pass: 'example_pass'
+      }
+    );
 
-  console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+    
+  } catch(err){
+    console.log('Connection to mongodb refused');
+  }
 };
 
 module.exports = connectDB;
