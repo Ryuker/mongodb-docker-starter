03c01bde853fd3c1489be96d70c432884473e199
ryuker <lifeisagamenotaproduct@gmail.com>
2024-06-16 18:05:28 +0200

    added dbService to connect to local mongodb

---
+++ server/services/dbService.js
@@ -0,0 +1,14 @@
+const mongoose = require('colors');
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(`mongodb://127.0.0.1:27017/AppDB`, {
+      user: 'mongo_admin',
+      pass: 'example_pass'
+    }
+  );
+
+  console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+module.exports = connectDB;
